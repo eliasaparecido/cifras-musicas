c728dc08daa1e4ed2ca1a3f8aac967ce75d042f5
Elias Aparecido Marciano <elias.marciano@al.ce.gov.br>
2026-04-02 16:12:28 -0300

    update vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -13,6 +13,9 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
+    allowedHosts: [
+      'cifras-musicas.codelabai.com.br',
+    ],
     watch: {
       usePolling: true,
     },
